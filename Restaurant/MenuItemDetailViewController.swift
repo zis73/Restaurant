3c59bdc15a20cd6a112f7dfb7df9f65f78b2e91b
Student <student@c001.local>
2018-11-13 11:25:20 +0300

    Created new 4 classes

---
+++ Restaurant/MenuItemDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuItemDetailViewController.swift
+//  Restaurant
+//
+//  Created by Student on 13/11/2018.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import UIKit
+
+class MenuItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
