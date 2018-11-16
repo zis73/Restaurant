1d86aae1c97fd24057f9a4481663ddf8521fd6d1
Student <student@c001.local>
2018-11-16 11:46:49 +0300

    Added completion(nil)

--- Restaurant/Controllers/MenuController.swift
+++ Restaurant/Controllers/MenuController.swift
@@ -15,8 +15,13 @@ class MenuController {
         let categoryURL = baseURL.appendingPathComponent("categories")
         let task = URLSession.shared.dataTask(with: categoryURL){
             data, response, error in
-            guard let data = data else { return }
-            guard let jsonDictionary = try? JSONSerialization.jsonObject(with: data) as? [String:Any] else { return }
+            guard let data = data else {
+                completion(nil)
+                return }
+            guard let jsonDictionary = try? JSONSerialization.jsonObject(with: data) as? [String:Any] else {
+                completion(nil)
+                return
+            }
             guard let categories = jsonDictionary?["categories"] as? [String] else { return }
             
             completion(categories)
